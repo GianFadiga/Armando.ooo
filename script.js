25efad408e1c0eb171693336cef647c84ca3bc30
GianFadiga <91998287+GianFadiga@users.noreply.github.com>
2023-11-21 21:17:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -33,6 +33,24 @@ function normalizeWord(word) {
   return word.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 }
 
+// Essa é a função que faz a construção do tabuleiro do jogo
+function initBoard() {
+  let board = document.getElementById("game-board");
+
+  for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
+    let row = document.createElement("div");
+    row.className = "letter-row";
+
+    for (let j = 0; j < 5; j++) {
+      let box = document.createElement("div");
+      box.className = "letter-box";
+      row.appendChild(box);
+    }
+
+    board.appendChild(row);
+  }
+}
+
 // Esta linha chama a função initBoard para inicializar o tabuleiro do jogo.
 initBoard();
 
